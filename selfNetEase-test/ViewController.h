1c00fe7ca232ddeb0b4d1dd54b6e654f6cd50b32
CranberryYam <hy1095@nyu.edu>
2016-01-30 10:20:22 -0500

    Initial Commit

---
+++ selfNetEase-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  selfNetEase-test
+//
+//  Created by yihl on 1/30/16.
+//  Copyright © 2016 yihl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ selfNetEase-testTests/selfNetEase_testTests.m
@@ -0,0 +1,39 @@
+//
+//  selfNetEase_testTests.m
+//  selfNetEase-testTests
+//
+//  Created by yihl on 1/30/16.
+//  Copyright © 2016 yihl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface selfNetEase_testTests : XCTestCase
+
+@end
+
+@implementation selfNetEase_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
